74ffc1140feed23ad3d8ee561d30d6407efb48a3
cibiebele <40744747+cibiebele@users.noreply.github.com>
2018-07-02 00:00:50 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# UdacityProject
+# Project: Pixel Art Maker
+
+This repository is for the Pixel Art Maker Project, which is part of the [Udacity Front-End Web Developer Nanodegree](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001) program.
+
+## The Project Details
+
+In this project, we are given a [starter template](https://github.com/udacity/project-pixel-art-maker-starter).  Using this template, I'm building the script to do the following:
+
+1. Upon form submission, build the grid.
+    - The form provides the number of rows (via the grid height) and columns (via the grid width).
+    - The grid is a `<table>`.
+2. Upon clicking on a single grid square, color that square's background color to the currently selected hex color.
+3. Build it with JavaScript (not jQuery).
+
+![Should look like this](https://d17h27t6h515a5.cloudfront.net/topher/2017/August/59a4a997_screen-shot-2017-08-22-at-8.33.49-pm/screen-shot-2017-08-22-at-8.33.49-pm.png)
+
+## Reviewer Note
+
+`///` indicates "thought process" or strategy for the code/project reviewer.
+
+## Disclaimer
+
+This repository is not for general use, but rather an exercise as part of an education program.
+
+Cheers and &lt;happy coding&gt;,    
+Tonya
---
+++ designs.js
@@ -0,0 +1,63 @@
+
+(function(document){
+    'use strict';
+
+    const elements = {
+        colorPicker: document.getElementById('colorPicker'),
+        gridCanvas: document.getElementById('pixel_canvas'),
+        widthInput: document.getElementById('input_width'),
+        heightInput: document.getElementById('input_height')
+    };
+
+ 
+    const init = function() {
+
+        document.getElementById('sizePicker').addEventListener('submit', makeGrid, false);
+
+        elements.gridCanvas.addEventListener('click', setGridColor);
+    };
+
+   
+    function makeGrid(event) {
+        event.preventDefault();
+
+        const gridSize = getGridSize();
+
+        clearCanvas();
+
+        for (let row = 0; row < gridSize.numberOfRows; row++) {
+            let tr = elements.gridCanvas.insertRow(row);
+
+            for (let col = 0; col < gridSize.numberOfColumns; col++) {
+                tr.insertCell(col);
+            }
+        }
+    }
+
+
+   
+    function setGridColor(event) {
+        let color = elements.colorPicker.value;
+
+        event.target.setAttribute('style', 'background-color: ' + color);
+    }
+
+   
+    function clearCanvas() {
+        elements.gridCanvas.innerHTML = '';
+    }
+
+    
+    function getGridSize() {
+        let numberOfRows = elements.heightInput.value;
+        let numberOfColumns = elements.widthInput.value;
+
+        return {
+            numberOfColumns: parseInt(numberOfColumns),
+            numberOfRows: parseInt(numberOfRows)
+        }
+    }
+
+    init();
+
+}(document));
